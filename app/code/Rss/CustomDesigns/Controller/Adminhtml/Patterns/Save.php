02aacfb3347efaef195e2979062afcc337d63b59
cralls <cralls@vectorns.com>
2026-03-06 08:13:06 -0700

    Update grid fields for Patterns Add Add controller for Patterns

--- app/code/Rss/CustomDesigns/Controller/Adminhtml/Patterns/Save.php
+++ app/code/Rss/CustomDesigns/Controller/Adminhtml/Patterns/Save.php
@@ -2,68 +2,85 @@
 namespace Rss\CustomDesigns\Controller\Adminhtml\Patterns;
 
 use Magento\Backend\App\Action;
-use Magento\Framework\Controller\Result\JsonFactory;
 use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Controller\Result\JsonFactory;
 
 class Save extends Action
 {
     const ADMIN_RESOURCE = 'Rss_CustomDesigns::custom_designs';
-
+    
     protected $jsonFactory;
     protected $resource;
-
+    
     public function __construct(
         Action\Context $context,
         JsonFactory $jsonFactory,
         ResourceConnection $resource
-    ) {
-        parent::__construct($context);
-        $this->jsonFactory = $jsonFactory;
-        $this->resource    = $resource;
+        ) {
+            parent::__construct($context);
+            $this->jsonFactory = $jsonFactory;
+            $this->resource = $resource;
     }
-
+    
     public function execute()
     {
-        $result = $this->jsonFactory->create();
-
-        $items = $this->getRequest()->getParam('items', []);
-
-        if (!count($items)) {
-            return $result->setData([
-                'error' => true,
-                'message' => __('No data to save.')
-            ]);
-        }
-
+        $request = $this->getRequest();
         $connection = $this->resource->getConnection();
         $table = $this->resource->getTableName('rss_custom_designs_patterns');
-
-        try {
-
-            foreach ($items as $patternId => $data) {
-
-                unset($data['pattern_id']);
-                unset($data['updated_at']);
-                unset($data['created_at']);
-
-                $connection->update(
-                    $table,
-                    $data,
-                    ['pattern_id = ?' => $patternId]
-                );
+        
+        if ($request->isXmlHttpRequest()) {
+            $result = $this->jsonFactory->create();
+            $items = $request->getParam('items', []);
+            
+            if (!count($items)) {
+                return $result->setData([
+                    'error' => true,
+                    'message' => __('No data to save.')
+                ]);
             }
-
-        } catch (\Exception $e) {
-
-            return $result->setData([
-                'error' => true,
-                'message' => $e->getMessage()
-            ]);
+            
+            try {
+                foreach ($items as $patternId => $data) {
+                    unset($data['pattern_id']);
+                    unset($data['updated_at']);
+                    unset($data['created_at']);
+                    
+                    $connection->update(
+                        $table,
+                        $data,
+                        ['pattern_id = ?' => $patternId]
+                        );
+                }
+                
+                return $result->setData([
+                    'error' => false,
+                    'message' => __('Saved successfully.')
+                ]);
+            } catch (\Exception $e) {
+                return $result->setData([
+                    'error' => true,
+                    'message' => $e->getMessage()
+                ]);
+            }
         }
-
-        return $result->setData([
-            'error' => false,
-            'message' => __('Saved successfully.')
-        ]);
+        
+        $data = [
+            'pattern_number'    => (string)$request->getParam('pattern_number'),
+            'version'           => (string)$request->getParam('version'),
+            'description'       => (string)$request->getParam('description'),
+            'art_templates_url' => (string)$request->getParam('art_templates_url'),
+            'http_address'      => (string)$request->getParam('http_address'),
+            'created_at'        => date('Y-m-d H:i:s'),
+            'updated_at'        => date('Y-m-d H:i:s')
+        ];
+        
+        try {
+            $connection->insert($table, $data);
+            $this->messageManager->addSuccessMessage(__('Pattern saved.'));
+        } catch (\Exception $e) {
+            $this->messageManager->addErrorMessage($e->getMessage());
+        }
+        
+        return $this->_redirect('customdesigns/patterns/index');
     }
 }
